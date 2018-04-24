d3ce75a1c510056b668011ce84bef2f2fad5298e
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2018-04-24 16:32:25 +0900

    add build script

--- README.md
+++ README.md
@@ -11,20 +11,7 @@ $ yarn
 ビルドする
 
 ```sh
-$ yarn build
-```
-
-Mac 用のビルド
-
-```sh
-$ yarn dist-mac
-```
-
-Windows 用のビルド（ wine が必要）
-
-```sh
-$ yarn dist-windows64 # 64bit用
-$ yarn dist-windows32 # 32bit用
+$ ./build.sh
 ```
 
 ## 開発の仕方
---
+++ build.sh
@@ -0,0 +1,17 @@
+SCRIPT_DIR=`dirname $0`
+
+yarn build
+
+rm -rf $SCRIPT_DIR/dist
+rm -rf $SCRIPT_DIR/dist-mac
+rm -rf $SCRIPT_DIR/dist-windows32
+rm -rf $SCRIPT_DIR/dist-windows64
+
+yarn dist-mac
+mv $SCRIPT_DIR/dist $SCRIPT_DIR/dist-mac
+
+yarn dist-windows32
+mv $SCRIPT_DIR/dist $SCRIPT_DIR/dist-windows32
+
+yarn dist-windows64
+mv $SCRIPT_DIR/dist $SCRIPT_DIR/dist-windows64
