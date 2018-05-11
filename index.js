10a3f1a98ff79e91798d0b2cef6be9d11c1ac083
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2018-05-11 22:47:14 +0900

    sending buffer in divided chunks

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const Serialport = require('serialport');
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
-const { Logger } = require("./Logger");
+const { Logger } = require('./Logger');
+
+const MAX_SUB_BUFFER_SIZE = 128;
 
 let mainWindow;
 let logger;
@@ -32,19 +34,39 @@ ipcMain.on('load-port-info-list', (event, data) => {
 
 ipcMain.on('send-color-data', (event, data, comName) => {
   // シリアルポートで色データを送る
-  logger.log('シリアルポートで送信開始');
+  function sendChunk(sendingIndex) {
+    if (sendingIndex >= buffer.length) {
+      port.close();
+      return;
+    }
+    const subBufferSize = Math.min(buffer.length - sendingIndex, MAX_SUB_BUFFER_SIZE);
+    const subBuffer = Buffer.from(buffer, sendingIndex, subBufferSize);
+    port.write(buffer, err => {
+      if (err) {
+        logger.log('送信中にエラーが起きました');
+        logger.log(err.message);
+      } else {
+        logger.log(`${sendingIndex + subBufferSize}[bytes] 送信完了`);
+      }
+      sendChunk(sendingIndex + subBufferSize);
+    });
+  }
   const buffer = Buffer.from(data);
   const port = new Serialport(comName);
-  port.write(buffer, err => {
-    if(err) {
-      logger.log("送信中にエラーが起きました");
-      logger.log(err.message);
-    } else {
-      logger.log("送信完了");
-    }
-    port.close();
+
+  port.on('error', err => {
+    logger.log('送信中にエラーが起きました');
+    logger.log(err.message);
     event.sender.send('send-color-data-completed');
   });
+
+  port.on('close', err => {
+    logger.log('送信終了');
+    event.sender.send('send-color-data-completed');
+  });
+
+  logger.log(`送信開始 合計バイト数: ${buffer.length}[bytes]`);
+  sendChunk(0);
 });
 
 ipcMain.on('save-state', (event, stateString) => {
