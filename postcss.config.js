f9d687fe7afbc90c0552a7e03d8facb1f5e23d31
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2018-05-17 13:27:45 +0900

    remove postcss packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("autoprefixer", {
-      browsers: [
-        "ie >= 10",
-        "ios >= 8",
-        "android >= 4.0",
-      ],
-    }),
-    require("css-mqpacker"),
-    require("cssnano"),
   ],
 }
